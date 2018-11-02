de4454539f3eab09eef1e3e601ef36a59e91bfe9
Theonilah Owali <theonilahtash@gmail.com>
2018-11-02 15:20:15 +0300

    corrected indentation errors in credentials.py file

--- credentials.py
+++ credentials.py
@@ -1,15 +1,20 @@
-class Credentials
-  """
-  Class that generates new instances of credentials
-  """
+class Credentials:
+    """
+    Class that generates new instances of credentials
+    """
 
-  credential_list = [] # empty credential list
+    credential_list = [] # empty credential list
 
-def __init__(self,user_name,phone_number,email,password):
+    def __init__(self,user_name,phone_number,email,password):
 
-
+        """
         user_name: New credential username.
         number: New credential phone number. 
         email: New credential email adress.
         password: New credential password.
-         
+        """
+
+        self.user_name = user_name
+        self.phone_number = phone_number
+        self.email = email
+        self.password = password       
