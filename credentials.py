ee1be5de4134b98aba784cdf76caf1db24a1a234
Theonilah Owali <theonilahtash@gmail.com>
2018-11-02 13:07:11 +0300

    added instances of my class to my credential.py

---
+++ credentials.py
@@ -0,0 +1,15 @@
+class Credentials
+  """
+  Class that generates new instances of credentials
+  """
+
+  credential_list = [] # empty credential list
+
+def __init__(self,user_name,phone_number,email,password):
+
+
+        user_name: New credential username.
+        number: New credential phone number. 
+        email: New credential email adress.
+        password: New credential password.
+         
