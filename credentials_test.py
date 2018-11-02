470bf8bf76cfffa83a9858f26c459c4f8ff24d3b
Theonilah Owali <theonilahtash@gmail.com>
2018-11-02 15:21:48 +0300

    rectified errors related to indentation to make my tests to pass

--- credentials_test.py
+++ credentials_test.py
@@ -1,31 +1,31 @@
 import unittest # Importing the unittest module
 from credentials import Credentials # Importing the credential class
-import pyperclip
-from user import User
+#import pyperclip
+#from user import User
 
 class TestCredential(unittest.TestCase):
 
-      '''
-      Test class that defines test cases for the credential class behaviours.
+    '''
+    Test class that defines test cases for the credential class behaviours.
 
-     Args:
-        unittest.TestCase: TestCase class that helps in creating test cases
-      '''    
+    Args:
+    unittest.TestCase: TestCase class that helps in creating test cases
+    '''    
 
-   def setUp(self):
-      '''
-      set up method to run before each test cases.
-      '''
-      self.new_credential = Credential("theonilahtash","0733445566","theonilahtash@gmail.com","tash1234") # create credential object
+    def setUp(self):
+        '''
+        set up method to run before each test cases.
+        '''
+        self.new_credential = Credentials("theonilahtash","0733445566","theonilahtash@gmail.com","tash1234") # create credential object
 
-   def test_init(self):
-      '''
-      test_init test case to test if the object is initialized properly
-      '''
-      self.assertEqual(self.new_credential.user_name,"theonilahtash")
-      self.assertEqual(self.new_credetial.phone_number,"0733445566") 
-      self.assertEqual(self.new_credential.email,"theonilahtash@gmail.com")
-      self.assertEqual(self.new_credential.password,"tash1234")
+    def test_init(self):
+        '''
+        test_init test case to test if the object is initialized properly
+        '''
+        self.assertEqual(self.new_credential.user_name,"theonilahtash")
+        self.assertEqual(self.new_credential.phone_number,"0733445566") 
+        self.assertEqual(self.new_credential.email,"theonilahtash@gmail.com")
+        self.assertEqual(self.new_credential.password,"tash1234")
 
 
 if __name__ == '__main__':
