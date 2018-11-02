0978f439b9237479d531e73b69c299d984343fe8
Theonilah Owali <theonilahtash@gmail.com>
2018-11-02 13:10:25 +0300

    imported a python test framework called unittest and added arguments to it

---
+++ credentials_test.py
@@ -0,0 +1,11 @@
+import unittest # Importing the unittest module
+from credentials import Credentials # Importing the credential class
+
+class TestCredential(unittest.TestCase):
+
+    '''
+    Test class that defines test cases for the credential class behaviours.
+
+    Args:
+        unittest.TestCase: TestCase class that helps in creating test cases
+    '''    
