8238a1c3d0ce69a6548de983346fd72c6343c89f
Theonilah Owali <theonilahtash@gmail.com>
2018-11-03 12:10:57 +0300

    added a class user that generates new instances for the user

---
+++ user/user.py
@@ -0,0 +1,29 @@
+class User:
+    '''
+    This is a class that generates new instances for the user in question
+    '''
+    user_list = []
+
+    def __init__(self,login_email,password):
+        '''
+        This method defines properties for our user object
+        '''
+        self.login_email = login_email
+        self.password = password
+
+
+    def save_user(self):
+        '''
+        This is a method which allows the user to save input
+        '''
+
+        User.user_list.append(self)
+
+    @classmethod
+    def user_login (cls, login):
+        '''
+        This is a method in which a user can check his/her credentials
+        '''
+        for User in cls.user_list:
+            if User.login_email == login:
+                return User 
